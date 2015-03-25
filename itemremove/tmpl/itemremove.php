e3943be405fd95a54ee6e8845ff28ce595fabce6
Attavus M.D. <attavus@users.noreply.github.com>
2015-03-25 15:35:13 +0200

    Update itemremove.php

--- itemremove/tmpl/itemremove.php
+++ itemremove/tmpl/itemremove.php
@@ -5,8 +5,8 @@
 * @version   3.3.0 March 2015
 * @author    Attavus M.D. http://www.raslab.org
 * @copyright Copyright (C) 2013-2015 R.A.S.Lab[.org]
-* @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
-*******************************************************************/
+* @license   http://opensource.org/licenses/GPL-2.0 GNU/GPLv2 only
+*****************************************************************/
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');		
@@ -16,4 +16,4 @@ defined('_JEXEC') or die('Restricted access');
 	<a href="javascript: if(confirm('Are you sure you want to remove this item?')) { window.location='<?php echo $this->app->link(array('controller' => 'submission', 'submission_id' => $submission_id, 'task' => 'remove', 'item_id' => $item_id, 'Itemid' => $this->app->menu->getActive()->id)); ?>';}" title="<?php echo JText::_('Remove Item'); ?>" class="item-icon remove-item">
 		<img src="<?php echo JRoute::_($this->app->path->url('assets:images/delete.png')); ?>" width="16" height="16" alt="<?php echo JText::_('Remove Item'); ?>" />
 	</a>
-<?php endif; ?>	
+<?php endif;
